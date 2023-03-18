5fa1ea70a19f4b851475323f99f214cbaada9bf9
chard2023 <richard.vistal012@gmail.com>
2023-03-18 11:25:11 +0800

    update cron

--- controllers/updateExpiredPromoCodes.js
+++ controllers/updateExpiredPromoCodes.js
@@ -1,6 +1,6 @@
 const updateExpiredPromoCodes = require('../utils/promoCodeCron');
 
-module.exports = async (req, res) => {
+const promoCodeStatus = async (req, res) => {
   try {
     await updateExpiredPromoCodes();
     res.status(200).send('Expired promo codes updated successfully');
@@ -9,3 +9,5 @@ module.exports = async (req, res) => {
     res.status(500).send('Error updating expired promo codes');
   }
 };
+
+module.exports = promoCodeStatus;
